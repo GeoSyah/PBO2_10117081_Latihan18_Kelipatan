cd9047f4e1453a90911f3d8182cbdfae17ca58a4
Geo Syah Alkautsar <43498342+GeoSyah@users.noreply.github.com>
2018-10-01 19:58:31 +0700

    Add files via upload

---
+++ src/pbo2_10117081_latihan18_kelipatan/PBO2_10117081_Latihan18_Kelipatan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_10117081_latihan18_kelipatan;
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * NIM : 10117081
+ * Kelas : IF2/PBO2
+ * Deskripsi Program : Kelipatan dari suatu bilangan menggunakan perulangan While
+ */
+public class PBO2_10117081_Latihan18_Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double i = 3.5;
+       
+       while(i <= 35) {
+           System.out.println(i);
+           
+           i = i + 3.5;
+       }
+    }
+    
+}
